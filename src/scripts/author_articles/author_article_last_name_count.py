43599a22f1f5ed5dc3b3c4d1b1167ce5d2985474
Dwijesh Bhageerutty <neerav789@gmail.com>
2014-10-27 16:25:44 -0400

    Minor change to the code to avoid article names with commas in them. This messes up the display when outputting to a csv file. Commas in article names are replaced by semicolons

--- src/scripts/author_articles/author_article_last_name_count.py
+++ src/scripts/author_articles/author_article_last_name_count.py
@@ -9,7 +9,7 @@ Generates a table with <article name, author name, last name count>
 
 from author.galenet.author_name_parser import get_authors
 from util.io import get_lines
-from scripts import GALENET_AUTHOR_MASTER_LIST_FNAME, AUTHOR_ARTICLE_LNC_CSV_FNAME, CITATIONS_FILE_FNAME
+from scripts import GALENET_AUTHOR_MASTER_LIST_FNAME, AUTHOR_ARTICLE_LNC_CSV_FNAME, CITATIONS_FILE_FNAME, ARTICLES_FOLDER
 import time, os
 from table.author_article import name_bigram_present, get_last_name_count, generate_bigram_combos
 from corpus.jstor.citations_parser import get_citations
@@ -26,9 +26,6 @@ lines = get_lines(GALENET_AUTHOR_MASTER_LIST_FNAME)
 authors = get_authors(lines)
 
 print ("#authors: " + str(len(authors)))
-
-# folder where articles are found
-articles_folder = "/Volumes/New HD/data/Lang_Lit_Lang_Lit_Bigrams/bigrams_expanded/"
 
 # list of words to be ignored
 ignore_list = ["the", "of", "de", "th", "la", "le", "", "in", "an"]
@@ -47,7 +44,7 @@ print ("Citations loaded.\n")
 with open(AUTHOR_ARTICLE_LNC_CSV_FNAME, 'w') as fd:
     fd.write("article id, author, LNC, article title\n")
     
-    file_names = sorted(os.listdir(articles_folder))
+    file_names = sorted(os.listdir(ARTICLES_FOLDER))
 
     for article in file_names:
         if article == ".DS_Store":
@@ -55,7 +52,7 @@ with open(AUTHOR_ARTICLE_LNC_CSV_FNAME, 'w') as fd:
         
         print (article)
         
-        article_content = open(articles_folder + article).read()
+        article_content = open(ARTICLES_FOLDER + article).read()
         
         max_auth_tup = (None,-1)
         
@@ -78,6 +75,6 @@ with open(AUTHOR_ARTICLE_LNC_CSV_FNAME, 'w') as fd:
         print (str(max_auth_tup[0]) + ' -> ' + str(max_auth_tup[1]))
         
         # output row <article file name, author name, number of occurrences of author's last name, article title>
-        fd.write(article + ',' + str(max_auth_tup[0]) + ',' + str(max_auth_tup[1]) + ',' + citations_dict[article].title + '\n')
+        fd.write(article + ',' + str(max_auth_tup[0]) + ',' + str(max_auth_tup[1]) + ',' + citations_dict[article].title.replace(',', ';') + '\n')
 
 print ("Done:" + str(time.clock()))
--- src/scripts/author_articles/author_article_table.py
+++ src/scripts/author_articles/author_article_table.py
@@ -9,7 +9,7 @@ Script to generate author-article table in a csv file
 from author.author import get_authors
 from util.io import get_lines
 from table.author_article import save_author_articles_to_file, last_name_and_one_first_name_present, last_name_present, name_bigram_present, find_authors
-from scripts import AUTHOR_NODE_LIST_FOLDER, AUTHOR_ARTICLE_CSV_FNAME
+from scripts import AUTHOR_NODE_LIST_FOLDER, AUTHOR_ARTICLE_CSV_FNAME, ARTICLES_FOLDER
 import os, time
 
 BIGRAMS = True # are we using bigrams data or unigrams?
@@ -26,19 +26,16 @@ for filee in os.listdir(AUTHOR_NODE_LIST_FOLDER):
 # parse lines and get author names
 authors = get_authors(lines)
 
-# folder where articles are found
-articles_folder = "ARTICLES FOLDER PATH"
-
 # list of authors for which we can only use last names
 last_name_only_list = "goethe balzac proust woolf dickens melville poe diderot flaubert verne stendhal zola fontane hoelderlin kafka novalis rilke schiller wieland"
 
 # get author article table
-file_names = sorted(os.listdir(articles_folder))
+file_names = sorted(os.listdir(ARTICLES_FOLDER))
 
 author_articles = dict()
 
 for article in file_names:
-    article_content = open(articles_folder + article).read()
+    article_content = open(ARTICLES_FOLDER + article).read()
     
     if not BIGRAMS:
         auths = find_authors(authors, article_content, last_name_and_one_first_name_present, last_name_present, last_name_only_list)
