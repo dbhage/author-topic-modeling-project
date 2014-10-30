1fd04b18589a5f59e41eefd7311f4e3269fb466c
Dwijesh Bhageerutty <neerav789@gmail.com>
2014-10-30 12:00:01 -0400

    parametrized generate_language_tables script. Added some documentation as well.

--- src/scripts/generate_languages_table.py
+++ src/scripts/generate_languages_table.py
@@ -11,14 +11,15 @@ import os
 
 MAIN_LANGUAGE = "english"
 
-FOLDER_NAME = "/home/dbhage/piperlab/language_test/"
-OUTPUT_FILE = "/home/dbhage/piperlab/language_test/language_table.csv"
+FOLDER_NAME = "" # path to folder containing text files on which we are to run language presence
+OUTPUT_FILE = FOLDER_NAME + "/language_table.csv" # or specify your own
 
 if os.path.exists(OUTPUT_FILE):
     os.remove(OUTPUT_FILE)
 
 master_table = []
 
+# for each file, get the language proportions
 for filee in os.listdir(FOLDER_NAME):
     fname = FOLDER_NAME + filee
     with open(fname, 'r') as fd:
@@ -27,11 +28,15 @@ for filee in os.listdir(FOLDER_NAME):
     table = percentage_stopwords(count_stopwords(text))
     master_table.append((filee.replace(',', '$'), table))
 
+# output master table
 if master_table:
     with open(OUTPUT_FILE, 'w') as fd:
+        # main language first in list of languages
         languages = master_table[0][1].keys()
         languages.remove(MAIN_LANGUAGE)
         languages = [MAIN_LANGUAGE] + languages
         fd.write(',' + ','.join(languages) + '\n')
+
+        # output rows        
         for (filee, table) in master_table:
             fd.write(filee + ',' + ','.join([str(table[k]) for k in languages]) + '\n')    
